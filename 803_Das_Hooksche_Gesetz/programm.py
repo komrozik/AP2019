8c9ed18bc807a8faa80fd3bed3fc21e033d335fd
Marcel Kebekus <marcel.kebekus@tu-dortmund.de>
2019-10-24 16:54:55 +0200

    803 add file for array of D

--- 803_Das_Hooksche_Gesetz/programm.py
+++ 803_Das_Hooksche_Gesetz/programm.py
@@ -4,18 +4,20 @@ from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-x,F,dx = np.genfromtxt('data.txt', unpack =True)
+#Datenimport:
+
+x,F = np.genfromtxt("data.txt",unpack=True)
 x=x*0.01 #skalierung in Meter
-x_werte= unp.uarray(x,dx)
-F_werte= unp.uarray(F,0.01*F)
+#x_werte= unp.uarray(x,dx)
+F_werte= unp.uarray(F,0.025*F)
+
+#Rechnungen: 
+
 def Federkonstante(x,F):
-    return F/x_werte
+    return F/x
 
 def Mittelwert(y):
     return (sum(y))/len(y)
-
-##def Standardabweichung(y):
-##    unp.std(y)
 
 def StandardabweichungdesMittelwert(y):
     return Standardabweichung(y)/np.sqrt(len.y)
@@ -23,6 +25,8 @@ def StandardabweichungdesMittelwert(y):
 def fit(x,a,b):
     return a*x+b
 
+
+#Curve Fit und Plot:
 l = np.linspace(0,0.6,1000)
 
 #params,cov = curve_fit(fit,x,F)
@@ -34,34 +38,34 @@ errors = np.sqrt(np.diag(covariance_matrix))
 plt.plot(
     l,
     params[0] * l + params[1],
+    'c--',
     label='Lineare Regression',
     linewidth=3,
 )
 
+
+#Ausgaben:
 print("Federkonstanten:")
-D = Federkonstante(x_werte,F)
-print (Federkonstante(x_werte,F))
+D = Federkonstante(x,F_werte)
+#np.savetxt("Federkonstanten.csv",[D],delimiter=" ",fmt='%d')
+
+print (Federkonstante(x,F_werte))
 print("Steigung:")
 print (params)
 
 print("Mittelwerten der Federkonstanten:")
-print(Mittelwert(Federkonstante(x_werte,F)))
-##print(Standardabweichung(x_werte))
-##print(StandardabweichungdesMittelwert(x_werte))
+print(Mittelwert(Federkonstante(x,F_werte)))
 
-
-
-for name, value, error in zip('ab', params, errors):
-    print(f'{name} = {value:.3f} ± {error:.3f}')
 
 
 #------------------------------------------------------------
 #Plot
 
-plt.errorbar(x,F,xerr=dx,yerr=stds(F_werte), fmt="x",label = 'Messwerte')
+plt.errorbar(x,F,yerr=stds(F_werte), fmt="rx",label = 'Messwerte',ecolor=['grey'])
 plt.xlabel(r'$ \Delta x / \mathrm{m}$')
 plt.ylabel(r'$F / \mathrm{N}$')
 plt.legend(loc="best")
+#plt.figure(figsize=(4.76,2.94))
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('build/plot.pdf')
 
