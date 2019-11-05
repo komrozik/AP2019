a09b76fcd11888011a1f4e0ab9ecd81bc744f4b5
Marcel Kebekus <marcel.kebekus@tu-dortmund.de>
2019-11-05 12:03:23 +0100

    802 plot.py in progress - plot the two function in 2 diffrent pdfs

--- 802_Fouriersynthese/plot.py
+++ 802_Fouriersynthese/plot.py
@@ -7,21 +7,25 @@ import sympy as sym
 #    (A(k)*np.cos(w(k)*x)+B(k)*sin(w(k)*x))
 
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
 
-# in matplotlibrc leider (noch) nicht möglich
+x= np.linspace(0,2*np.pi,1000)
+y= abs(np.sin(x))
+
+plt.plot(x,y, label=r"$|\sin(x)|$")
+plt.xlim(0,2*np.pi+1)
+plt.ylim(0,1.25)
+plt.xticks([0,np.pi/2,np.pi,3*np.pi/2,2*np.pi],[r"$0$",r"$\frac{1}{2}\pi$",r"$\pi$",r"$\frac{3}{2}\pi$",r"$2\pi$"])
+plt.yticks([0,0.5,1],[0,0.5,1])
+plt.xlabel(r"x")
+plt.ylabel(r"y")
+plt.legend(loc="best")
+
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+plt.savefig('build/plot1.pdf')
+
+x=np.linespace(-np.pi,np.pi,1000)
+y=x
+
+plt.plot(x,y,label=r"$x$")
