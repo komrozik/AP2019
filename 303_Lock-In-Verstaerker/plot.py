7c52bbdd51e98b0b5a02647bea3884b9462de28e
Marcel Kebekus <marcel.kebekus@tu-dortmund.de>
2020-01-06 19:49:52 +0100

    303 plot - curve fit, tryed and failed

--- 303_Lock-In-Verstaerker/plot.py
+++ 303_Lock-In-Verstaerker/plot.py
@@ -14,8 +14,25 @@ from scipy.optimize import curve_fit
 #5) LED | d=distanc, V=Spannung
 d,V=np.genfromtxt("data3.txt", unpack = True)
 d=d-18.6
+kehr_V=1/V
+i=0
+while(i<len(d)):
+    print(f"{d[i]} & {d[i+1]} \\\\ \n")
+    i=i+1
+
+
+x_plot = np.linspace(0,36)
+def function(x,a,b):
+    return (a*1/(x**2))+b
+params, covariance_matrix = curve_fit(function,d,V)
+errors = np.sqrt(np.diag(covariance_matrix))
+plt.plot(x_plot,function(x_plot,*params),label='Fit')
+
 plt.plot(d,V,"rx",label="Messwerte")
 plt.ylabel(f"Spannung $U \;/\;V$")
 plt.xlabel(f"Entfernung$\;/\;cm$")
+plt.legend()
 plt.savefig('build/plotLED.pdf',bbox_inches='tight')
+plt.close()
+
 #plt.show()
