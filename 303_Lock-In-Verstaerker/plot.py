b09591394b561c92544254427567280e7c6fa489
Konstantin Mrozik <konstantin.mrozik@udo.edu>
2020-01-06 21:31:11 +0100

    303 content plot.py - fixed spelling mistakes and added new plots

--- 303_Lock-In-Verstaerker/plot.py
+++ 303_Lock-In-Verstaerker/plot.py
@@ -11,20 +11,20 @@ from scipy.optimize import curve_fit
 
 
 
-#5) LED | d=distanc, V=Spannung
+#5) LED | d=distance, V=Spannung
 d,V=np.genfromtxt("data3.txt", unpack = True)
-d=d-18.6
+d=d
 i=0
-while(i<=len(d)-1):
-    print(f"{d[i]} & {V[i]} \\\\ \n")
-    i=i+1
+# while(i<=len(d)-1):
+#     print(f"{d[i]} & {V[i]} \\\\ \n")
+#     i=i+1
 
 
 x_plot = np.linspace(18,55)
 def function(x,a,b):
     return (a*1/(x**2))+b
 params, covariance_matrix = curve_fit(function,d,V)
-#print(f"Parameter: {params}")
+print(f"Parameter: {params}")
 #print(f"Spannung: {V}")
 errors = np.sqrt(np.diag(covariance_matrix))
 plt.plot(x_plot-18.5,function(x_plot,*params),label='Fit')
@@ -34,5 +34,49 @@ plt.ylabel(f"Spannung $U \;/\;V$")
 plt.xlabel(f"Entfernung$\;/\;cm$")
 plt.legend()
 plt.savefig('build/plotLED.pdf',bbox_inches='tight')
+#plt.show()
+plt.close()
+
+#2) Werte ohne Rauschen | p = phase, V = Spannung
+p,V=np.genfromtxt("data1.txt", unpack = True)
+# while(i<=len(p)-1):
+#     print(f"{p[i]} & {V[i]} \\\\ \n")
+#     i=i+1
+
+x_plot = np.linspace(0,360)
+def function(x,a,c):
+    return (2/np.pi)*a*np.cos((x/180)*np.pi+c)
+params, covariance_matrix = curve_fit(function,p,V)
+print(f"Parameter: {params}")
+#print(f"Spannung: {V}")
+errors = np.sqrt(np.diag(covariance_matrix))
+plt.plot(x_plot,function(x_plot,*params),label='Fit')
+plt.plot(p,V,"rx",label="Messwerte")
+plt.ylabel(f"Spannung $U \;/\;V$")
+plt.xlabel(f"Phase$\;/\;°$")
+plt.legend()
+plt.savefig('build/plot1.pdf',bbox_inches='tight')
+#plt.show()
+plt.close()
+
+#3) Werte ohne Rauschen | p = phase, V = Spannung
+p,V=np.genfromtxt("data2.txt", unpack = True)
+# while(i<=len(p)-1):
+#     print(f"{p[i]} & {V[i]} \\\\")
+#     i=i+1
+
+x_plot = np.linspace(0,360)
+def function(x,a,c):
+    return (2/np.pi)*a*np.cos((x/180)*np.pi+c)
+params, covariance_matrix = curve_fit(function,p,V)
+print(f"Parameter: {params}")
+#print(f"Spannung: {V}")
+errors = np.sqrt(np.diag(covariance_matrix))
+plt.plot(x_plot,function(x_plot,*params),label='Fit')
+plt.plot(p,V,"rx",label="Messwerte")
+plt.ylabel(f"Spannung $U \;/\;V$")
+plt.xlabel(f"Phase$\;/\;°$")
+plt.legend()
+plt.savefig('build/plot2.pdf',bbox_inches='tight')
 #plt.show()
 plt.close()
