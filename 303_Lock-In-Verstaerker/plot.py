6396985e31bee02995c6030df21434d0457addf3
Marcel Kebekus <marcel.kebekus@tu-dortmund.de>
2020-01-06 20:32:20 +0100

    stuff2

--- 303_Lock-In-Verstaerker/plot.py
+++ 303_Lock-In-Verstaerker/plot.py
@@ -14,11 +14,11 @@ from scipy.optimize import curve_fit
 #5) LED | d=distanc, V=Spannung
 d,V=np.genfromtxt("data3.txt", unpack = True)
 d=d-18.6
-kehr_V=1/V
 i=0
-while(i<len(d)-1):
-    print(f"{d[i]} & {d[i+1]} \\\\ \n")
+while(i<=len(d)-1):
+    print(f"{d[i]} & {V[i]} \\\\ \n")
     i=i+1
+
 
 
 x_plot = np.linspace(0,36)
@@ -26,8 +26,10 @@ def function(x,a,b):
     return (a*1/(x**2))+b
 params, covariance_matrix = curve_fit(function,d,V)
 errors = np.sqrt(np.diag(covariance_matrix))
-plt.plot(x_plot,function(x_plot,*params),label='Fit')
+unparams = unp.uarray(params,errors)
+print(f"a={unparams[0]} b={unparams[1]}")
 
+plt.plot(x_plot,function(x_plot,*params),label='Fit')
 plt.plot(d,V,"rx",label="Messwerte")
 plt.ylabel(f"Spannung $U \;/\;V$")
 plt.xlabel(f"Entfernung$\;/\;cm$")
