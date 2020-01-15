b43cf406d36d67206bd29c883b5e4529a428a667
Marcel Kebekus <marcel.kebekus@tu-dortmund.de>
2020-01-16 00:12:39 +0100

    254 plot.py - add header

---
+++ 354_Gedämpfte_und_erzwungene_Schwinungen/plot.py
@@ -0,0 +1,11 @@
+import matplotlib
+matplotlib.use('Qt4Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import sympy as sym
+from scipy import integrate
+import uncertainties.unumpy as unp 
+from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
+from scipy.optimize import curve_fit
+
+
