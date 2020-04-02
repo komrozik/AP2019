98353e45ea551077ac6db3ecdcc8dc2d595e40b1
Marcel Kebekus <marcel.kebekus@tu-dortmund.de>
2020-04-02 19:34:18 +0200

    Überschrift --> KOMPLETTE Erkläreung letzter commit

--- 000_Zusatzversuch/plot.py
+++ 000_Zusatzversuch/plot.py
@@ -133,7 +133,6 @@ R5,F5_0= polyfit(L0A,FA5,1)
 
 
 F0_true=[F1_0+L1_0_mittelwert*R1,F2_0+L2_0_mittelwert*R2,F3_0+L3_0_mittelwert*R3,F4_0+L4_0_mittelwert*R4,F5_0+L5_0_mittelwert*R5]
-print(f"F_0 TRUE:{F0_true}")
 
 #R mit dF und dL
 #--------------------------
@@ -146,12 +145,11 @@ R5=(F51_mittelwert-F52_mittelwert)/(L1_z-L2_z)
 Fx_0=[F1_0,F2_0,F3_0,F4_0,F5_0]
 R=[R1,R2,R3,R4,R5]
 
+#Schnittpunkt xS für diskussionsgraphic
 xS_1=(-F2_0+F3_0)/(R2-R3)
 xS_2=(-F4_0+F5_0)/(R4-R5)
 
 xN=[-Fx_0[0]/R[0],-Fx_0[3]/R[3],-Fx_0[4]/R[4],]
-print(f"xS_1:f({xS_1})={R2*xS_1+F2_0} --> BASIS: f({xS_1})={unparamsR1[0]*xS_1+unparamsR1[1]}")
-print(f"xS_2:f({xS_2})={R2*xS_2+F2_0} --> BASIS: f({xS_2})={unparamsR1[0]*xS_2+unparamsR1[1]}")
 
 
 
@@ -259,7 +257,7 @@ plt.show()
 plt.close()
 
 
-#federkonstanten R
+#federkonstanten R und F1_0 was dem Verschiebungswert v entspricht
 R=[R1,R2,R3,R4,R5]
 print(f"""
     R1: {R1}, F1_0: {F1_0}
@@ -268,40 +266,48 @@ print(f"""
     R4: {R4}, F4_0: {F4_0}
     R5: {R5}, F5_0: {F5_0}
 """)
+#innere Vorspannkraft
+print(f"F_0 TRUE:{F0_true}")
+#Schnittpunk überprüfung
+print(f"xS_1:f({xS_1})={R2*xS_1+F2_0} --> BASIS: f({xS_1})={unparamsR1[0]*xS_1+unparamsR1[1]}")
+print(f"xS_2:f({xS_2})={R2*xS_2+F2_0} --> BASIS: f({xS_2})={unparamsR1[0]*xS_2+unparamsR1[1]}")
+
+
 #ausgabe Tabel
-print(D1[5])
 print(f"""
-    $M$ & {MA[0]} & {MA[1]} & {MA[2]} & {MA[3]} & {MA[4]} \\\\
-    $D_a$ & {D1[0]} & {D2[0]} & {D3[0]} & {D4[0]} & {D5[0]} \\\\
-          & {D1[1]} & {D2[1]} & {D3[1]} & {D4[1]} & {D5[1]} \\\\
-          & {D1[2]} & {D2[2]} & {D3[2]} & {D4[2]} & {D5[2]} \\\\
-          & {D1[3]} & {D2[3]} & {D3[3]} & {D4[3]} & {D5[3]} \\\\
-          & {D1[4]} & {D2[4]} & {D3[4]} & {D4[4]} & {D5[4]} \\\\
-          & {D1[5]} &         &         &         &         \\\\
-    $bar(D_a)$ & {D1_mittelwert} & {D2_mittelwert} & {D3_mittelwert} & {D4_mittelwert} & {D5_mittelwert}\\\\
-    $D_(a_S)$ {D1_standabw} $ {D2_standabw} $ {D3_standabw} $ {D4_standabw} $ {D5_standabw}\\\\
-    $L_a$ & {L1_0[0]} & {L2_0[0]} & {L3_0[0]} & {L4_0[0]} & {L5_0[0]} \\\\
-          & {L1_0[1]} & {L2_0[1]} & {L3_0[1]} & {L4_0[1]} & {L5_0[1]} \\\\
-          & {L1_0[2]} & {L2_0[2]} & {L3_0[2]} & {L4_0[2]} & {L5_0[2]} \\\\
-          & {L1_0[3]} & {L2_0[3]} & {L3_0[3]} & {L4_0[3]} & {L5_0[3]} \\\\
-          & {L1_0[4]} & {L2_0[4]} & {L3_0[4]} & {L4_0[4]} & {L5_0[4]} \\\\
-          & {L1_0[5]} &         &         &         &         \\\\
-    $bar(L_a)$ & {L1_0_mittelwert} & {L2_0_mittelwert} & {L3_0_mittelwert} & {L4_0_mittelwert} & {L5_0_mittelwert}\\\\
-    $L_(a_S)& {L1_standabw} & {L2_standabw} & {L3_standabw} & {L4_standabw} & {L5_standabw}\\\\
-    $F1$ bei $L1={L1_z}$ & {F11[0]} & {F21[0]} & {F31[0]} & {F41[0]} & {F51[0]}\\\\
-                         & {F11[1]} & {F21[1]} & {F31[1]} & {F41[1]} & {F51[1]}\\\\
-                         & {F11[2]} & {F21[2]} & {F31[2]} & {F41[2]} & {F51[2]}\\\\
-                         & {F11[3]} & {F21[3]} & {F31[3]} & {F41[3]} & {F51[3]}\\\\
-                         & {F11[4]} & {F21[4]} & {F31[4]} & {F41[4]} & {F51[4]}\\\\
-    $bar(F1)$ & {F11_mittelwert} & {F21_mittelwert} & {F31_mittelwert} & {F41_mittelwert} & {F51_mittelwert}\\\\
-    $F2$ bei $L2={L2_z}$ & {F12[0]} & {F22[0]} & {F32[0]} & {F42[0]} & {F52[0]}\\\\
-                         & {F12[1]} & {F22[1]} & {F32[1]} & {F42[1]} & {F52[1]}\\\\
-                         & {F12[2]} & {F22[2]} & {F32[2]} & {F42[2]} & {F52[2]}\\\\
-                         & {F12[3]} & {F22[3]} & {F32[3]} & {F42[3]} & {F52[3]}\\\\
-                         & {F12[4]} & {F22[4]} & {F32[4]} & {F42[4]} & {F52[4]}\\\\
-    $bar(F1)$ & {F12_mittelwert} & {F22_mittelwert} & {F32_mittelwert} & {F42_mittelwert} & {F52_mittelwert}\\\\
-    $R$ & {R1} & {R2} & {R3} & {R4} & {R5} \\\\
-    $F_0$ & {F1_0} & {F2_0} & {F3_0} & {F4_0} & {F5_0} \\\\
+    $M$ \t& {MA[0]} \t& {MA[1]} \t& {MA[2]} \t& {MA[3]} \t& {MA[4]} \t\\\\
+    $D_a$ \t& {D1[0]} \t& {D2[0]} \t& {D3[0]} \t& {D4[0]} \t& {D5[0]} \t\\\\
+          \t& {D1[1]} \t& {D2[1]} \t& {D3[1]} \t& {D4[1]} \t& {D5[1]} \t\\\\
+          \t& {D1[2]} \t& {D2[2]} \t& {D3[2]} \t& {D4[2]} \t& {D5[2]} \t\\\\
+          \t& {D1[3]} \t& {D2[3]} \t& {D3[3]} \t& {D4[3]} \t& {D5[3]} \t\\\\
+          \t& {D1[4]} \t& {D2[4]} \t& {D3[4]} \t& {D4[4]} \t& {D5[4]} \t\\\\
+          \t& {D1[5]} \t&         \t&         \t&         \t&         \t\\\\
+    $bar(D_a)$ \t& {D1_mittelwert} \t& {D2_mittelwert} \t& {D3_mittelwert} \t& {D4_mittelwert} \t& {D5_mittelwert}\t\\\\
+    $D_(a_S)$ {D1_standabw} \t& {D2_standabw} \t& {D3_standabw} \t& {D4_standabw} \t& {D5_standabw}\t\\\\
+    $L_a$ \t& {L1_0[0]} \t& {L2_0[0]} \t& {L3_0[0]} \t& {L4_0[0]} \t& {L5_0[0]} \t\\\\
+          \t& {L1_0[1]} \t& {L2_0[1]} \t& {L3_0[1]} \t& {L4_0[1]} \t& {L5_0[1]} \t\\\\
+          \t& {L1_0[2]} \t& {L2_0[2]} \t& {L3_0[2]} \t& {L4_0[2]} \t& {L5_0[2]} \t\\\\
+          \t& {L1_0[3]} \t& {L2_0[3]} \t& {L3_0[3]} \t& {L4_0[3]} \t& {L5_0[3]} \t\\\\
+          \t& {L1_0[4]} \t& {L2_0[4]} \t& {L3_0[4]} \t& {L4_0[4]} \t& {L5_0[4]} \t\\\\
+          \t& {L1_0[5]} \t&         \t&         \t&         \t&         \t\\\\
+    $bar(L_a)$ \t& {L1_0_mittelwert} \t& {L2_0_mittelwert} \t& {L3_0_mittelwert} \t& {L4_0_mittelwert} \t& {L5_0_mittelwert}\t\\\\
+    $L_(a_S)\t& {L1_standabw} \t& {L2_standabw} \t& {L3_standabw} \t& {L4_standabw} \t& {L5_standabw}\t\\\\
+    $F1$ bei $L1={L1_z}$ \t& {F11[0]} \t& {F21[0]} \t& {F31[0]} \t& {F41[0]} \t& {F51[0]}\t\\\\
+                         \t& {F11[1]} \t& {F21[1]} \t& {F31[1]} \t& {F41[1]} \t& {F51[1]}\t\\\\
+                         \t& {F11[2]} \t& {F21[2]} \t& {F31[2]} \t& {F41[2]} \t& {F51[2]}\t\\\\
+                         \t& {F11[3]} \t& {F21[3]} \t& {F31[3]} \t& {F41[3]} \t& {F51[3]}\t\\\\
+                         \t& {F11[4]} \t& {F21[4]} \t& {F31[4]} \t& {F41[4]} \t& {F51[4]}\t\\\\
+    $bar(F1)$ \t& {F11_mittelwert} \t& {F21_mittelwert} \t& {F31_mittelwert} \t& {F41_mittelwert} \t& {F51_mittelwert}\t\\\\
+    $F2$ bei $L2={L2_z}$ \t& {F12[0]} \t& {F22[0]} \t& {F32[0]} \t& {F42[0]} \t& {F52[0]}\t\\\\
+                         \t& {F12[1]} \t& {F22[1]} \t& {F32[1]} \t& {F42[1]} \t& {F52[1]}\t\\\\
+                         \t& {F12[2]} \t& {F22[2]} \t& {F32[2]} \t& {F42[2]} \t& {F52[2]}\t\\\\
+                         \t& {F12[3]} \t& {F22[3]} \t& {F32[3]} \t& {F42[3]} \t& {F52[3]}\t\\\\
+                         \t& {F12[4]} \t& {F22[4]} \t& {F32[4]} \t& {F42[4]} \t& {F52[4]}\t\\\\
+    $bar(F1)$ \t& {F12_mittelwert} \t& {F22_mittelwert} \t& {F32_mittelwert} \t& {F42_mittelwert} \t& {F52_mittelwert}\\\\
+    $R$ \t& {R1} \t& {R2} \t& {R3} \t& {R4} \t& {R5} \\\\
+    $v0$ \t& {F1_0} \t& {F2_0} \t& {F3_0} \t& {F4_0} \t& {F5_0} \\\\
+    $F0$ \t& {F0_true[0]} \t& {F0_true[1]} \t& {F0_true[2]} \t& {F0_true[3]} \t& {F0_true[4]} \\\\
+
 """)
 
 
