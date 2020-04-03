7ac4741444121192bca7a970b08a7080b5f6dc35
Marcel Kebekus <marcel.kebekus@tu-dortmund.de>
2020-04-03 12:28:58 +0200

    wrong merged - replace all wrong merged files with sicherheitskopie

--- 000_Zusatzversuch/plot.py
+++ 000_Zusatzversuch/plot.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import numpy as np
 import uncertainties.unumpy as unp 
 from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
@@ -454,20 +453,3 @@ plt.show()
 
 
 
-||||||| merged common ancestors
-=======
-import numpy as np
-import uncertainties.unumpy as unp 
-from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
-import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
-
-#Datenimport:
-
-D,L_0 = np.genfromtxt("data1.1.txt",unpack=True)
-D_mittelwert = sum(D)/len(D)
-L_0_mittlewert = sum(L_0)/len(L_0)
-F1,F2 = np.genfromtxt("data2.2.txt",unpack = True)
-F1_mittelwert = sum(F1)/len(F1)
-F2_mittelwert = sum(F2)/len(F2)
->>>>>>> 4725ae2bea169a0e96b27f3c21c1fb3c50c57da5
